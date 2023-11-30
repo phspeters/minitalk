327107ebc142943076b64d6605fde1836e4a3250
Pedro Henrique De Souza Peters <pehenri2@c2r8p3.42sp.org.br>
2023-11-30 17:53:13 -0300

    starting off the project

---
+++ server.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pehenri2 <pehenri2@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 12:19:15 by pehenri2          #+#    #+#             */
+/*   Updated: 2023/11/30 17:52:02 by pehenri2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	ft_printf("Server PID: %d\n", getpid());
+	ft_bzero(&sa, sizeof(struct sigaction));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
+	sa.sa_sigaction = &handle_client_signal;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+void	handle_client_signal(int signum, siginfo_t *info, void *context)
+{
+	
+}
