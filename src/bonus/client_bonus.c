99a06d9f9d99a41867762b39c3db296fffea29a5
Peters <peters@archlinux>
2023-12-04 22:25:03 -0300

    server bonus not printing the message correctly

--- src/bonus/client_bonus.c
+++ src/bonus/client_bonus.c
@@ -6,39 +6,17 @@
 /*   By: pehenri2 <pehenri2@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 09:58:43 by pehenri2          #+#    #+#             */
-/*   Updated: 2023/12/04 18:49:34 by pehenri2         ###   ########.fr       */
+/*   Updated: 2023/12/04 22:23:57 by pehenri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	send_bit(int pid, char *str)
+void	handle_client_error(char *str)
 {
-	static int	bits_sent = 0;
-	static char	*message;
-
-	if (*str)
-		message = ft_strdup(str);
-	if (!message)
-		handle_error(message);
-	if (message[bits_sent / 8])
-	{
-		if ((*message << (bits_sent % 8)) & 0b10000000)
-			if (kill(pid, SIGUSR1) == -1)
-				handle_error(message);
-		else
-			if (kill(pid, SIGUSR2) == -1)
-				handle_error(message);
-		bits_sent++;
-		return (0);
-	}
-	if (!send_message_end(server_pid, message))
-		return (0);
-	else
-		bits_sent += send_message_end(server_pid, message);
-	free(message);
-	ft_printf("Sent %i bits (%i bytes) to server", bits_sent, (bits_sent / 8));
-	return (1);
+	if (str)
+		free (str);
+	exit(EXIT_FAILURE);
 }
 
 int	send_message_end(int pid, char *str)
@@ -49,33 +27,55 @@ int	send_message_end(int pid, char *str)
 	if (bits_sent < 16)
 	{
 		if ((*end << (bits_sent % 8)) & 0b10000000)
-			if (kill(pid, SIGUSR1) == -1)
-				handle_error(str);
-		else
+		{
 			if (kill(pid, SIGUSR2) == -1)
-				handle_error(str);
+				handle_client_error(str);
+		}
+		else
+			if (kill(pid, SIGUSR1) == -1)
+				handle_client_error(str);
 		bits_sent++;
 		return (0);
 	}
 	return (bits_sent);
 }
 
-void	handle_server_signal(int signum, siginfo_t *info, void *context)
+int	send_bit(int server_pid, char *str)
 {
-	int	end;
+	static int	bits_sent = 0;
+	static char	*message;
 
-	(void)context;
-	if (signum == SIGUSR1)
-		end = send_bit(info->si_pid, 0);
-	if (end)
-		exit(EXIT_SUCCESS);
+	if (str)
+		message = ft_strdup(str);
+	if (!message)
+		handle_client_error(message);
+	if (message[bits_sent / 8])
+	{
+		if ((message[bits_sent / 8] << (bits_sent % 8)) & 0b10000000)
+		{
+			if (kill(server_pid, SIGUSR2) == -1)
+				handle_client_error(message);
+		}
+		else
+			if (kill(server_pid, SIGUSR1) == -1)
+				handle_client_error(message);
+		bits_sent++;
+		return (0);
+	}
+	if (!send_message_end(server_pid, message))
+		return (0);
+	bits_sent += 16;
+	free(message);
+	exit(ft_printf("Sent %i bits (%i bytes) to server\n", bits_sent, (bits_sent / 8)));
 }
 
-void	handle_error(char *str)
+void	handle_server_signal(int signum, siginfo_t *info, void *context)
 {
-	if (str)
-		free (str);
-	exit(EXIT_FAILURE);
+	(void)context;
+	if (signum == SIGUSR1)
+		send_bit(info->si_pid, 0);
+	else if (signum == SIGUSR2)
+		handle_client_error(NULL);
 }
 
 int	main(int argc, char *argv[])
@@ -85,8 +85,8 @@ int	main(int argc, char *argv[])
 	if (argc != 3)
 		return (write(STDOUT_FILENO, "Correct usage: ./client 'Server PID' 'message'\n", 47));
 	ft_bzero(&action, sizeof(struct sigaction));
-	sa.sa_flags = SA_RESTART | SA_SIGINFO;
-	sa.sa_sigaction = &handle_server_signal;
+	action.sa_flags = SA_RESTART | SA_SIGINFO;
+	action.sa_sigaction = &handle_server_signal;
 	if (sigaction(SIGUSR1, &action, NULL) == -1 || sigaction(SIGUSR2, &action, NULL) == -1)
 		return (write(STDOUT_FILENO, "Error setting up signal handler\n", 32));
 	send_bit(ft_atoi(argv[1]), argv[2]);
--- src/bonus/server_bonus.c
+++ src/bonus/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pehenri2 <pehenri2@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 12:19:15 by pehenri2          #+#    #+#             */
-/*   Updated: 2023/12/04 18:48:14 by pehenri2         ###   ########.fr       */
+/*   Updated: 2023/12/04 22:10:11 by pehenri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ void	handle_client_signal(int signum, siginfo_t *info, void *context)
 	static int				bits_read = 0;
 
 	(void)context;
-	if (signum == SIGUSR2)
-		c = c | 0;
+	if (signum == SIGUSR1)
+		c ^= 0x80 >> bits_read;
+	else if (signum == SIGUSR2)
+		c |= 0x80 >> bits_read;
 	c = c << 1;
 	bits_read++;
 	if (bits_read % 8 == 0)
@@ -33,20 +35,22 @@ void	handle_client_signal(int signum, siginfo_t *info, void *context)
 		{
 			bits_read = 0;
 			c = 0b11111111;
+			return ;
 		}
 	}
 	if (kill(info->si_pid, SIGUSR1) == -1)
-		handle_error();
+		return ;
 }
 
+//ft_printf("Received %i bits (%i bytes) from client\n", bits_read, (bits_read / 8));
 int	main(void)
 {
 	struct sigaction	action;
 
 	ft_bzero(&action, sizeof(struct sigaction));
 	action.sa_flags = SA_RESTART | SA_SIGINFO;
-	sa.sa_sigaction = &handle_client_signal;
-	if (sigaction(SIGUSR1, &action, NULL) == -1 || sigaction(SIGUSR2, &actions, NULL) == -1)
+	action.sa_sigaction = &handle_client_signal;
+	if (sigaction(SIGUSR1, &action, NULL) == -1 || sigaction(SIGUSR2, &action, NULL) == -1)
 	{
 		perror("Signal");
 		exit(errno);
